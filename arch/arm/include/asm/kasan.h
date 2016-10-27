83f28faef1bb613acce861b1c760d886059841b5
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2016-10-27 16:04:35 +0900

    arm: temporary enable HAVE_ARCH_KASAN
    For checking to compile
    
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>

---
+++ arch/arm/include/asm/kasan.h
@@ -0,0 +1,31 @@
+#ifndef _ASM_ARM_KASAN_H
+#define _ASM_ARM_KASAN_H
+
+#include <asm/memory.h>
+
+/* 128MB for area higher than PAGE_OFFSET and 2MB for 16MB module area */
+#define KASAN_SHADOW_SIZE      UL(SZ_128M + SZ_2M)
+
+/*
+ * MODULES_VADDR is the lowest address that kernel can access. See
+ * help text for KASAN_SHADOW_OFFSET in lib/Kconfing.kasan.
+ */
+#define KASAN_SHADOW_START     (KASAN_SHADOW_OFFSET + \
+                               (MODULES_VADDR >> KASAN_SHADOW_SCALE_SHIFT))
+#define KASAN_SHADOW_END       (KASAN_SHADOW_START + KASAN_SHADOW_SIZE)
+
+#ifndef __ASSEMBLY__
+
+#ifdef CONFIG_KASAN
+
+void __init kasan_init(void);
+
+#else
+
+static inline void kasan_init(void) { }
+
+#endif
+
+#endif
+
+#endif
