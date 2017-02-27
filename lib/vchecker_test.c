1e04ae0aeff6267fd466ecc7d251b361bc7ee28a
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2017-02-27 11:41:43 +0900

    vchecker_test: check if the vchecker works well with allocated object
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>

--- lib/vchecker_test.c
+++ lib/vchecker_test.c
@@ -9,9 +9,11 @@ struct object {
 };
 
 static struct kmem_cache *s;
-static struct delayed_work dwork;
+static struct delayed_work dwork_new_obj;
+static struct delayed_work dwork_old_obj;
+static void *old_obj;
 
-static void workfn(struct work_struct *work)
+static void workfn_new_obj(struct work_struct *work)
 {
 	struct object *obj;
 	struct delayed_work *dwork = (struct delayed_work *)work;
@@ -28,19 +30,39 @@ static void workfn(struct work_struct *work)
 	mod_delayed_work(system_wq, dwork, HZ);
 }
 
+static void workfn_old_obj(struct work_struct *work)
+{
+	struct object *obj = old_obj;
+	struct delayed_work *dwork = (struct delayed_work *)work;
+
+	obj->v[0] = 7;
+	obj->v[1] = 5;
+	obj->v[2] = 9;
+	obj->v[1] = 4;
+	obj->v[2] = obj->v[1];
+
+	mod_delayed_work(system_wq, dwork, HZ);
+}
+
 static int __init vchecker_test_init(void)
 {
-	INIT_DELAYED_WORK(&dwork, workfn);
+	INIT_DELAYED_WORK(&dwork_new_obj, workfn_new_obj);
+	INIT_DELAYED_WORK(&dwork_old_obj, workfn_old_obj);
 	s = kmem_cache_create("vchecker_test",
 			sizeof(struct object), 0, SLAB_NOLEAKTRACE, NULL);
-	mod_delayed_work(system_wq, &dwork, HZ);
+	mod_delayed_work(system_wq, &dwork_new_obj, HZ);
+
+	old_obj = kmem_cache_alloc(s, GFP_KERNEL);
+	mod_delayed_work(system_wq, &dwork_old_obj, HZ);
 
 	return 0;
 }
 
 static void __exit vchecker_test_fini(void)
 {
-	cancel_delayed_work_sync(&dwork);
+	cancel_delayed_work_sync(&dwork_new_obj);
+	cancel_delayed_work_sync(&dwork_old_obj);
+	kmem_cache_free(s, old_obj);
 	kmem_cache_destroy(s);
 }
 
