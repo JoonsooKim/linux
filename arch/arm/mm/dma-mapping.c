79e819f015940a8bc546b7c0cb3d73f011be0439
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2017-11-06 17:14:31 +0900

    arm/dma: flush i-cache and d-cache separately
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -485,8 +485,7 @@ void __init dma_contiguous_remap(void)
 	if (!dma_mmu_remap_num)
 		return;
 
-	/* Flush full cache since CMA area would be large */
-	flush_cache_all();
+	__flush_icache_all();
 	for (i = 0; i < dma_mmu_remap_num; i++) {
 		phys_addr_t start = dma_mmu_remap[i].base;
 		phys_addr_t end = start + dma_mmu_remap[i].size;
@@ -502,6 +501,8 @@ void __init dma_contiguous_remap(void)
 		map.virtual = __phys_to_virt(start);
 		map.length = end - start;
 		map.type = MT_MEMORY_DMA_READY;
+
+		__cpuc_flush_dcache_area((void *)map.virtual, map.length);
 
 		/*
 		 * Clear previous low-memory mapping to ensure that the
